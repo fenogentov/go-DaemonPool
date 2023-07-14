89ed67b0bfa47c26c10dadee5ea57c4f32816ce8
fenogentov <ShuraF@yandex.ru>
2023-07-14 08:37:16 +0300

    WIP

---
+++ Run.go
@@ -0,0 +1,45 @@
+package godaemonpool
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"runtime/debug"
+
+	"golang.org/x/exp/slog"
+)
+
+var ErrNumberWorkers = errors.New("invalid number of workers")
+
+// PoolDo
+func PoolDo(ctx context.Context, name string, maxWorkers int, job func(ctx context.Context)) error {
+	if maxWorkers < 1 {
+		return ErrNumberWorkers
+	}
+
+	ch := make(chan struct{}, maxWorkers)
+
+	for {
+		select {
+		case ch <- struct{}{}:
+			go func(ctx context.Context, ch chan struct{}) {
+				defer func() {
+					if p := recover(); p != nil {
+						msg := fmt.Sprintf("panic '%s'", name)
+						slog.Error(msg, "recover", p, "stack", string(debug.Stack()))
+					}
+					<-ch
+				}()
+				job(ctx)
+			}(ctx, ch)
+
+		case <-ctx.Done():
+			msg := fmt.Sprintf("shutdown %s", name)
+			slog.Info(msg)
+			return ctx.Err()
+
+		default:
+			continue
+		}
+	}
+}
